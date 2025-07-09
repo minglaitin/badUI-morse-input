067dfa48fc7e7c4808cc541c190b2c07669eb019
Lily Ming <lilyming2000@gmail.com>
2025-07-09 17:00:50 +0800

    implement translating morse code to text

--- script.js
+++ script.js
@@ -1,9 +1,68 @@
-let display = document.getElementById("morse-input");
+let morseInput = document.getElementById("morse-input");
+let translatedInput = document.getElementById("translated-input");
 let mouse = document.getElementById("morse-key");
 let startTime, duration;
-let input = "";
+let letterCode = "";
+let decodedResult = "";
+let timeoutID = 0;
+
+const MORSE_CODE = {
+    ".-": "a",
+    "-...": "b",
+    "-.-.": "c",
+    "-..": "d",
+    ".": "e",
+    "..-.": "f",
+    "--.": "g",
+    "....": "h",
+    "..": "i",
+    ".---": "j",
+    "-.-": "k",
+    ".-..": "l",
+    "--": "m",
+    "-.": "n",
+    "---": "o",
+    ".--.": "p",
+    "--.-": "q",
+    ".-.": "r",
+    "...": "s",
+    "-": "t",
+    "..-": "u",
+    "...-": "v",
+    ".--": "w",
+    "-..-": "x",
+    "-.--": "y",
+    "--..": "z",
+    ".----": "1",
+    "..---": "2",
+    "...--": "3",
+    "....-": "4",
+    ".....": "5",
+    "-....": "6",
+    "--...": "7",
+    "---..": "8",
+    "----.": "9",
+    "-----": "0"
+};
+
+function decodeChar(code) {
+    let newChar = MORSE_CODE[code];
+    
+    // if invalid morse code
+    if (!newChar) {
+        newChar = "";
+    }
+
+    decodedResult = decodedResult + newChar;
+    translatedInput.innerHTML = decodedResult;
+
+    // reset current input
+    letterCode = "";
+    morseInput.innerHTML = letterCode;
+}
 
 function startHandler() {
+    clearTimeout(timeoutID);
     startTime = Date.now();
 }
 
@@ -12,12 +71,19 @@ function endHandler() {
 
     // if short click then dot, if long press then dash
     if (duration < 200) {
-        input = input + ".";
+        letterCode = letterCode + ".";
     } else {
-        input = input + "-";
+        letterCode = letterCode + "-";
     }
-    display.innerHTML = input;
+    morseInput.innerHTML = letterCode;
+
+    // 1 sec between each character
+    timeoutID = setTimeout(() => {
+        decodeChar(letterCode);
+    }, 1000);
 }
+
 
 mouse.onpointerdown = startHandler;
 mouse.onpointerup = endHandler;
+
