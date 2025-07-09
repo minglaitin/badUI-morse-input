b0c871d8e58c2c90bc6c1def3f14bb14a9400602
Lily Ming <lilyming2000@gmail.com>
2025-07-09 14:54:47 +0800

    determine dot or dash based on duration of mouse click

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 let display = document.getElementById("morse-input");
 let mouse = document.getElementById("morse-key");
-let startTime, endTime;
+let startTime, duration;
+let input = "";
 
 function startHandler() {
     startTime = Date.now();
 }
 
 function endHandler() {
-    endTime = Date.now();
-    display.innerHTML = `You pressed ${endTime - startTime} milliseconds`;
+    duration = Date.now() - startTime;
+
+    // if short click then dot, if long press then dash
+    if (duration < 200) {
+        input = input + ".";
+    } else {
+        input = input + "-";
+    }
+    display.innerHTML = input;
 }
 
 mouse.onpointerdown = startHandler;
